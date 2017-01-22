4b7d6b6b22a219081e10e140b97db9c914964fc6
Colin Harris <col.w.harris@gmail.com>
2017-01-22 09:58:18 +0800

    Initial Commit

---
+++ LexApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LexApp
+//
+//  Created by Colin Harris on 22/1/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
